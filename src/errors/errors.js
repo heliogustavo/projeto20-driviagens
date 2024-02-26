e396b5a61d42475baed9cac44356e0c429b3624d
heliogustavo <jacomehelioiq@gmail.com>
2024-02-26 10:32:18 -0400

    funções dos errors contruídas e configuradas

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -8,10 +8,48 @@
 
  function wrongData(){
     return{
-        type:"",
+        type:"wrongData",
         message: "The fields are wrong!"
     }
  }
 
+ function notFound (resource="item"){
+    return{
+            type:"notFound",
+            message:`${resource} not found!`
+        } 
+ }
 
-export const errors = {conflict, wrongData}
+ function equalCities (){
+    return{
+            type:"conflict",
+            message:`The destination can't be equal to the origin city!`
+        } 
+ }
+
+ function invalidFlightDate (){
+    return{
+            type:"wrongData",
+            message:`The flight date nedds to be greater than today!`
+        } 
+ }
+ function invalidTravelDate (){
+    return{
+            type:"wrongData",
+            message:`bigger-date needs to be greater than smaller-date!`
+        } 
+ }
+ function invalidTravelDateAmount (){
+    return{
+            type:"badRequest",
+            message:`Both bigger-date and smaller-date needs to be passed!`
+        } 
+ }
+ function tooManyResults (){
+    return{
+            type:"tooManyResults",
+            message:`Exeeded 10 travels limit!`
+        } 
+ } 
+
+export const errors = {conflict, wrongData, notFound, equalCities, invalidFlightDate, invalidTravelDate, invalidTravelDateAmount, tooManyResults}
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,5 +5,22 @@ export default function errorHandler(error, req, res, next){
     if(error.type === "conflict"){
         return res.status(httpStatus.CONFLICT).send(error.message);
     }
+
+    if(error.type === "notFound"){
+        return res.status(httpStatus.CONFLICT).send(error.message);
+    }
+
+    if(error.type === "wrongData"){
+        return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message);
+    }
+
+    if(error.type === "badRequest"){
+        return res.status(httpStatus.BAD_REQUEST).send(error.message);
+    }
+
+    if(error.type === "tooManyResults"){
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+    }
+
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Sorry, something went wrong");
 }
